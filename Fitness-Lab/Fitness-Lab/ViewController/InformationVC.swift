a4b47fe940ce26b08b62ec1dc16499c3424d8fa5
“Alice” <wl02722691@gmail.com>
2018-10-29 12:03:54 +0800

    fix ListVC scroll to bottom bug

--- Fitness-Lab/Fitness-Lab/ViewController/InformationVC.swift
+++ Fitness-Lab/Fitness-Lab/ViewController/InformationVC.swift
@@ -13,7 +13,6 @@ class InformationVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
